89eec1c60d249326d8eaf621d7bfb514891fbd88
yanxujun <yanxujun@yuantutech.com>
2018-06-25 18:20:18 +0800

    init

---
+++ app/src/main/java/com/yxj/dialog/MainActivity.java
@@ -0,0 +1,57 @@
+package com.yxj.dialog;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.SimpleAdapter;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn1).setOnClickListener(this);
+        btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn1:
+                String[] array = {"北京","上海","广州","深圳","杭州","上饶"};
+                new ShareDialog.Builder(this)
+                        .setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,array))
+                        .show();
+                break;
+            case R.id.btn2:
+                new ConfirmDialog.Builder(this)
+                        .setTitle("世界杯")
+                        .setBtnLeft("英格兰",(dialog)->{
+                            Log.e("yxj","btnLeft");
+                            dialog.dismiss();
+                        })
+                        .setBtnRight("德国",(dialog)->{
+                            Log.e("yxj","btnRight");
+                        })
+                        .setAnim(AnimType.Slidebottom)
+                        .setDuration(1000)
+                        .setGravity(Gravity.BOTTOM)
+                        .show();
+
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/yxj/dialog/ShareDialog.java
@@ -0,0 +1,48 @@
+package com.yxj.dialog;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+
+/**
+ * Author:  Yxj
+ * Time:    2018/6/25 下午4:43
+ * -----------------------------------------
+ * Description:
+ */
+public class ShareDialog extends BaseDialog {
+
+    protected ShareDialog(@NonNull Context context, int themeResId) {
+        super(context, themeResId);
+    }
+
+    public static class Builder extends BaseDialog.Builder{
+
+        public Builder(Context context) {
+            super(context);
+        }
+
+        private BaseAdapter adapter;
+
+        @Override
+        public int setContentView() {
+            return R.layout.dialog_share;
+        }
+
+        @Override
+        public void initView(View layout) {
+            GridView gd = layout.findViewById(R.id.gd);
+            if(adapter!=null){
+                gd.setAdapter(adapter);
+            }
+        }
+
+        public Builder setAdapter(BaseAdapter adapter){
+            this.adapter = adapter;
+            return this;
+        }
+
+    }
+}
